b293052454ea8ef45eb73d9df0ea1a9a11e728f5
ismaiel <ismaiel.nab@gmail.com>
2021-02-21 17:28:16 +0100

    do HomeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-function App() {
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screen/HomeScreen";
+
+const App = () => {
   return (
     <>
-      <h1>Ismaiel</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
